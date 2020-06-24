398b8cd0b99f767cd4a3fde78ac12488bb57c570
Yuki Sato <yuki.sato@lightcurve.io>
2020-06-24 20:11:12 +0200

    Add initial value to the state count

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import { bind } from "nd"
 
+const Counter = bind(({ count }) => <span>{count || 0}</span>, ["count"])
+
 export default bind(
   ({ set, count }) => (
     <div
       onClick={()=> set((count || 0) + 1, "count")}
     >
-      add count: {count　|| 0}
+      click to add count: <Counter />
     </div>
   ),
-  ["count"]
+  [{"count": 10 }]
 )
